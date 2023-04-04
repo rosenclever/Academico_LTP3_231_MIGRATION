34cb1cfc208a3b9247538aa3dcd3e9bb41edc7e6
rosenclever <rosenclever@gmail.com>
2023-04-04 20:54:20 -0300

    Configurado AcademicoContext com Instituição

---
+++ Academico/Data/AcademicoContext.cs
@@ -0,0 +1,13 @@
+﻿using Academico.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Academico.Data
+{
+    public class AcademicoContext : DbContext
+    {
+        public AcademicoContext(DbContextOptions options) : base(options)
+        {
+        }
+        DbSet<Instituicao> Instituicoes { get; set; }
+    }
+}
